bd3b23af796a55847b9c93eeec3715f1f6e7ed42
唐家林 <taogarfield@hotmail.com>
2019-03-11 21:20:32 +0800

    完善打包和打包后的路径问题

--- src/main/java/net/sudot/excel/datadictionary/Appcation.java
+++ src/main/java/net/sudot/excel/datadictionary/Appcation.java
@@ -3,7 +3,10 @@ package net.sudot.excel.datadictionary;
 import net.sudot.excel.datadictionary.dto.InParameter;
 import net.sudot.excel.datadictionary.excel.WriteWorkbook;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 /**
  * 程序入口
@@ -11,17 +14,23 @@ import java.util.Properties;
  * @author tangjialin on 2019-03-01.
  */
 public class Appcation {
+    private static final Pattern PATTERN = Pattern.compile("(/|\\d\\:).*");
 
     public static void main(String[] args) throws Exception {
         Properties properties = new Properties();
-        properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("in-parameter.txt"));
+        File file = new File(System.getProperty("sun.java.command"));
+        properties.load(new FileInputStream(new File(file.getParent(), "conf/in-parameter.txt")));
         InParameter inParameter = new InParameter();
         inParameter.setUrl(properties.getProperty("url"));
         inParameter.setUser(properties.getProperty("user"));
         inParameter.setPassword(properties.getProperty("password"));
         inParameter.setSchema(properties.getProperty("schema"));
         inParameter.setExcludeTablesString(properties.getProperty("exclude-tables-string"));
-        inParameter.setFilePath(properties.getProperty("file-path"));
+        String filePath = properties.getProperty("file-path");
+        if (!PATTERN.matcher(filePath).matches()) {
+            filePath = new File(file.getParent(), filePath).getCanonicalPath();
+        }
+        inParameter.setFilePath(filePath);
         WriteWorkbook.write(inParameter);
     }
 
